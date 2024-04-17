ffcec2b91de0cfbab76cbfdeffe376ed44b6b65c
Moh-Lecouz <86624116+Moh-Lecouz@users.noreply.github.com>
2024-04-17 16:53:45 +0100

    init

---
+++ py_parts/Gym_Environnement.py
@@ -0,0 +1,7 @@
+# Setting up the environment
+
+import gymnasium as gym
+env = gym.make('MsPacmanDeterministic-v0', full_action_space = False)
+state_shape = env.observation_space.shape
+state_size = env.observation_space.shape[0]
+number_actions = env.action_space.n
---
+++ py_parts/hyperparameters.py
@@ -0,0 +1,5 @@
+# Initializing the hyperparameters
+
+discount_factor = 0.99
+minibatch_size = 64
+learning_rate = 5e-4
